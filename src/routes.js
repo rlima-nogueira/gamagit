da3c6e16201fa0a5aaa07a4c0b77750099e32742
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-05-31 18:55:18 -0300

    erro na rota repositories

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,11 @@ export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route patch='/' exact component = { Home } />
+                <Route patch='/' exact component = { Home } /> 
                 <Route patch='/repositories' component = { Repositories } />
             </Switch>
         </BrowserRouter>
     )
 }
+
+//entender porque a rota de repositories não está funcionando. 
